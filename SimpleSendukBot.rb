ceabb377bb6b8ba959ea4b9d0164b2042f5da5d6
IronLaddle <abdtasnim0@gmail.com>
2017-08-08 07:05:06 +0800

    updata

--- SimpleSendukBot.rb
+++ SimpleSendukBot.rb
@@ -5,9 +5,9 @@ Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
     case message.text
     when '/start'
-      bot.api.send_message(chat_id: message.chat.id, text: "I am SendukBesi robot, Please click on this commands /intro /waktuSolat ..Keep calm n ")
+      bot.api.send_message(chat_id: message.chat.id, text: "Please click on this commands /intro /waktuSolat ..Keep calm n ")
     when '/intro'
-      bot.api.send_message(chat_id: message.chat.id, text: "Welcome to SendukBesi telegram robot (beta version)")
+      bot.api.send_message(chat_id: message.chat.id, text: "Welcome to telegram bot (beta version)")
     when '/waktuSolat'
       bot.api.send_message(chat_id: message.chat.id, text: "Imsak   : 5.41")
       bot.api.send_message(chat_id: message.chat.id, text: "Subuh   : 5.51")
